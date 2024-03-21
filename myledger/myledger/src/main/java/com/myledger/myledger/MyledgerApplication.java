a4e1dc77da957a341a3e1757fa06d9710396cc22
Komșa Attila <komsaatti@yahoo.com>
2024-03-21 20:19:12 +0200

    Init project

---
+++ myledger/myledger/src/main/java/com/myledger/myledger/MyledgerApplication.java
@@ -0,0 +1,13 @@
+package com.myledger.myledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyledgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyledgerApplication.class, args);
+	}
+
+}
