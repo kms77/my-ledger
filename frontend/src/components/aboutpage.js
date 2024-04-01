d193013c68871ffc3cc319b8a91fd8116ddccab7
Acatrinei Ștefan <stefan.aca74@gmail.com>
2024-04-01 15:23:10 +0300

    created .js file for the about page

---
+++ frontend/src/components/aboutpage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function aboutpage() {
+    return(
+        <h2>Clear Title for about page</h2>
+    );
+}
+
+export default aboutpage
