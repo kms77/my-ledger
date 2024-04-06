b0f8b6930eb8cc875ddb7ba030c13ceb4342715c
bianca-paula <bianca.paula2505@gmail.com>
2024-04-06 20:22:16 +0300

    Added tax calculation

--- frontend/src/visitors/calculator-taxe/CalculatorTaxe.jsx
+++ frontend/src/visitors/calculator-taxe/CalculatorTaxe.jsx
@@ -4,42 +4,55 @@ import { Stack,Image, Row, Col, Form} from "react-bootstrap";
 
 const CalculatorTaxe = () => {
 
-  const calculeaza = (venit) => {
+  const SALARIU_MINIM_BRUT = 3300;
+  const PLAFON_CASS = 60 * SALARIU_MINIM_BRUT;
 
-    // Contribuția de asigurări sociale (CAS)
-    // Plafonul minim brut pe economie în 2024 este 3.300 lei
-    const salariuMinimBrut = 3300;
-    const cas = Math.min(venit, salariuMinimBrut) * 0.25;
+  const calculeazaTaxe = (venitLunarBrut, cheltuieliDeductibile) => {
 
-    // Contribuția de asigurări sociale de sănătate (CASS)
-    // Se aplică un procent de 10% la norma de venit sau norma anuală de venit ajustată
-    const cass = venit * 0.1;
+    const venitAnualBrut = venitLunarBrut * 12;
 
-    // Impozitul pe venit (10% din venitul net)
-    const impozitVenit = (venit - cas - cass) * 0.1;
+    let cassAnual = 0.1 * venitAnualBrut;
 
-    // Calculul total al taxelor
-    const totalTaxe = impozitVenit + cas + cass;
+    if(cassAnual >= PLAFON_CASS){
+      cassAnual = PLAFON_CASS;
+    }
+    const contributieCASS = cassAnual / 12;
 
-    // Venitul net
-    const venitNet = venit - totalTaxe;
+    let contributieCAS = 0;
 
+    if(venitLunarBrut < 3300){
+      contributieCAS = venitLunarBrut * 0.25;
+    } else if (venitLunarBrut > 3300 && venitLunarBrut < 6600){
+      contributieCAS = 3300 * 0.25;
+    }
+    else{
+      contributieCAS = 6600 * 0.25;
+    }
+
+    const venitImpozitabil = venitLunarBrut - contributieCASS - contributieCAS - cheltuieliDeductibile;
+    const impozitPeVenit = venitImpozitabil * 0.1;
+
+    const venitNet = venitImpozitabil - impozitPeVenit;
+    
     return {
-        impozitVenit,
-        cas,
-        cass,
-        venitNet
-    };
-  }
+      impozitPeVenit,
+      contributieCAS,
+      contributieCASS,
+      venitNet
+  };
+
+  };
 
     const [venitLunar, setVenitLunar] = useState('');
+    const [cheltuieliDeductibile, setCheltuieliDeductibile] = useState('');
     const [taxeLuna, setTaxeLuna] = useState(null);
 
     const handleCalculeazaClick = (event) => {
         event.preventDefault();
         const venitLunarNumber = parseFloat(venitLunar);
-        if (!isNaN(venitLunarNumber)) {
-            const taxe = calculeaza(venitLunarNumber);
+        const cheltuieliDeductibileNumber = parseFloat(cheltuieliDeductibile);
+        if (!isNaN(venitLunarNumber) && !isNaN(cheltuieliDeductibileNumber)) {
+            const taxe = calculeazaTaxe(venitLunarNumber, cheltuieliDeductibileNumber);
             setTaxeLuna(taxe);
         } else {
             // Tratează cazul în care utilizatorul introduce un venit invalid
@@ -66,20 +79,33 @@ const CalculatorTaxe = () => {
                   onChange={(e) => setVenitLunar(e.target.value)}
                 />
 
+                <label>
+                  Cheltuieli deductibile (lei):
+                </label>
+                <input
+                  class="input-venit"
+                  type="number"
+                  value={cheltuieliDeductibile}
+                  onChange={(e) => setCheltuieliDeductibile(e.target.value)}
+                />
+
 
 
                 <button class="calculate-button" onClick={(event) => handleCalculeazaClick(event)}>Calculează</button>
 
               </Form>
+              <div className="mx-4">
               {taxeLuna && (
                 <div>
                   <h3>Rezultate:</h3>
-                  <p>Impozitul pe venit: <strong>{taxeLuna.impozitVenit} lei</strong></p>
-                  <p>Contribuția la pensie (CAS): <strong>{taxeLuna.cas} lei</strong></p>
-                  <p>Contribuția la sănătate (CASS): <strong>{taxeLuna.cass} lei</strong></p>
+                  <p>Impozitul pe venit: <strong>{taxeLuna.impozitPeVenit} lei</strong></p>
+                  <p>Contribuția la pensie (CAS): <strong>{taxeLuna.contributieCAS} lei</strong></p>
+                  <p>Contribuția la sănătate (CASS): <strong>{taxeLuna.contributieCASS} lei</strong></p>
                   <p>Venitul net: <strong>{taxeLuna.venitNet} lei</strong></p>
                 </div>
               )}
+              </div>
+
             </div>
         </Col>
       </Row>
