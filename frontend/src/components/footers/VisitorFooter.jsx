5d2d8dbc42c65189e5a5880a58132dd0037a7f2f
Komșa Attila <komsaatti@yahoo.com>
2024-04-06 12:15:04 +0300

    Added image

--- frontend/src/components/footers/VisitorFooter.jsx
+++ frontend/src/components/footers/VisitorFooter.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
-import { Row, Col } from "react-bootstrap";
 
 const VisitorFooter = () => {
     return (
-        <Row>
-            <Col>
-                <footer className="py-3 border-top">
-                        <div className="ps-5">
-                            <p className="text-muted m-1">@ 2024 My Ledger</p>
-                            <p className="text-muted m-1">Email: email@email.com</p>
-                            <p className="text-muted m-1">Telefon: 0799999999</p>
-                        </div>
-                </footer>
-            </Col>
-        </Row>
+        <footer className="d-flex py-3 border-top m-0">
+            <div className="ps-5">
+                <p className="text-muted m-1">@ 2024 My Ledger</p>
+                <p className="text-muted m-1">Email: email@email.com</p>
+                <p className="text-muted m-1">Telefon: 0799999999</p>
+            </div>
+        </footer>
     );
 };
 
--- frontend/src/visitors/contact/Contact.jsx
+++ frontend/src/visitors/contact/Contact.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Container, Row, Col, Form } from "react-bootstrap";
+import contactUsImage from '../../assets/images/contact_us.png';
 
 const Contact = () => {
 
     const renderSimpleTextField = (control, label, value) => {
         return (
             <Form.Group as={Row} controlId={control}>
-                <Form.Label column sm="5" className="d-flex">{label}</Form.Label>
+                <Form.Label column sm="5" className="d-flex align-items-center form-label-lg">{label}</Form.Label>
                 <Col>
                     <Form.Control type="text" className="form-control-lg text-color-white" plaintext readOnly defaultValue={value} />
                 </Col>
@@ -18,15 +19,27 @@ const Contact = () => {
         <Container>
             <Row className="my-5">
                 <Col lg={6} className="contact-image-column">
-
+                    <div className="d-flex justify-content-center align-items-center">
+                        <img 
+                            src={contactUsImage} 
+                            href="Contact us image" 
+                            className="contact-image" 
+                            style={{width: "20vw", minWidth: "300px"}}
+                        />
+                    </div>
                 </Col>
                 <Col lg={6} className="contact-details-column">
                     <div className="text-color-white px-4 my-4">
                         <h3>Contact</h3>
                         <Form id="contactFormID" className="my-4">
-                            {renderSimpleTextField("addressID", "Adresă:", "Cluj-Napoca")}
                             {renderSimpleTextField("emailID", "Email:", "email@email.com")}
                             {renderSimpleTextField("phoneID", "Telefon:", "0799999999")}
+                            <Form.Group as={Row} controlId="addressID">
+                                <Form.Label column sm="5" className="d-flex align-items-center">Adresă:</Form.Label>
+                                <Col>
+                                    <Form.Control as="textarea" rows={2} className="form-control-lg text-color-white" plaintext readOnly defaultValue="Cluj-Napoca, str. Mihail Kogălniceanu, nr. 1" />
+                                </Col>
+                            </Form.Group>
                         </Form>
                     </div>
                 </Col>
