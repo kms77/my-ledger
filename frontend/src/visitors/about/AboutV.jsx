db0e3e39426e9044080a532c87d6868471b91b1e
Acatrinei Ștefan <stefan.aca74@gmail.com>
2024-04-19 15:20:30 +0300

    cleaned up about visitor page

--- frontend/src/visitors/about/AboutV.jsx
+++ frontend/src/visitors/about/AboutV.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Stack } from "react-bootstrap";
-import securityImage from '../../assets/images/security.jpg';
+import { Stack, Row, Col, Form } from "react-bootstrap";
+import securityImage from "../../assets/images/security.jpg";
 
 const AboutV = () => {
   return (
     <div className="container about-container">
-      <img className="about-title-img" src={securityImage} alt="securityImage"/>
+      <img id="about-title-img" src={securityImage} alt="securityImage" />
       <Stack direction="horizontal" gap={3}>
-        
         <h1 className="display-4 mb-4 ">Despre Noi</h1>
       </Stack>
       <Stack className="styled-stack-blue">
@@ -32,28 +31,91 @@ const AboutV = () => {
       <Stack className="styled-stack-blue">
         <h2 className="display-4 mb-4 ">Valorile Noastre</h2>
         <br></br>
-        <ul className="list-group list-group-flush lead text-center" id="special-list">
+        <ul
+          className="list-group list-group-flush lead text-center"
+          id="special-list"
+        >
           <li className="list-group-item" id="about-page-item">
-            <i className="fas fa-check-circle me-2 text-primary"></i>{" "}
-            Simplitate: Credem că aplicația noastră ar trebui să fie ușor de
-            utilizat și înțeles de către toate PFA-urile, indiferent de
-            expertiza lor tehnică.
+            <Row className="my-5">
+              <Col lg={6} className="contact-image-column">
+                <div className="d-flex justify-content-center align-items-center">
+                  <img
+                    src={securityImage}
+                    href="Contact us image"
+                    className="contact-image"
+                    style={{ width: "20vw", minWidth: "300px" }}
+                    alt="securityImage"
+                  />
+                </div>
+              </Col>
+              <Col lg={6} className="contact-image-column">
+                <i className="fas fa-check-circle me-2 text-primary"></i>{" "}
+                Simplitate: Credem că aplicația noastră ar trebui să fie ușor de
+                utilizat și înțeles de către toate PFA-urile, indiferent de
+                expertiza lor tehnică.
+              </Col>
+            </Row>
           </li>
           <li className="list-group-item" id="about-page-item">
-            <i className="fas fa-lock me-2 text-success"></i> Securitate: Luăm
-            foarte în serios securitatea datelor și folosim cele mai recente
-            tehnologii pentru a vă proteja informațiile.
+            <Row className="my-5">
+              <Col lg={6} className="contact-image-column">
+                <i className="fas fa-lock me-2 text-success"></i> Securitate:
+                Luăm foarte în serios securitatea datelor și folosim cele mai
+                recente tehnologii pentru a vă proteja informațiile.
+              </Col>
+              <Col lg={6} className="contact-details-column">
+                <div className="d-flex justify-content-center align-items-center">
+                  <img
+                    src={securityImage}
+                    href="Contact us image"
+                    className="contact-image"
+                    style={{ width: "20vw", minWidth: "300px" }}
+                    alt="securityImage"
+                  />
+                </div>
+              </Col>
+            </Row>
           </li>
           <li className="list-group-item" id="about-page-item">
-            <i className="fas fa-dollar-sign me-2 text-warning"></i>{" "}
-            Accesibilitate: Credem că aplicația noastră ar trebui să fie
-            accesibilă pentru toate PFA-urile, indiferent de bugetul lor.
+            <Row className="my-5">
+              <Col lg={6} className="contact-image-column">
+                <div className="d-flex justify-content-center align-items-center">
+                  <img
+                    src={securityImage}
+                    href="Contact us image"
+                    className="contact-image"
+                    style={{ width: "20vw", minWidth: "300px" }}
+                    alt="securityImage"
+                  />
+                </div>
+              </Col>
+              <Col lg={6} className="contact-image-column">
+                <i className="fas fa-headset me-2 text-info"></i>
+                Accesibilitate: Credem că aplicația noastră ar trebui să fie
+                accesibilă pentru toate PFA-urile, indiferent de bugetul lor.
+              </Col>
+            </Row>
           </li>
           <li className="list-group-item" id="about-page-item">
-            <i className="fas fa-headset me-2 text-info"></i> Asistență: Ne
-            angajăm să oferim clienților noștri o asistență excelentă. Echipa
-            noastră este întotdeauna aici pentru a vă ajuta cu orice întrebări
-            pe care le-ați putea avea.
+            <Row className="my-5">
+              <Col lg={6} className="contact-image-column">
+                <i className="fas fa-headset me-2 text-info"></i> Asistență: Ne
+                angajăm să oferim clienților noștri o asistență excelentă.
+                Echipa noastră este întotdeauna aici pentru a vă ajuta cu orice
+                întrebări pe care le-ați putea avea.
+              </Col>
+              <Col lg={6} className="contact-details-column">
+                <div className="d-flex justify-content-center align-items-center">
+                  <img
+                    src={securityImage}
+                    href="Contact us image"
+                    className="contact-image"
+                    style={{ width: "20vw", minWidth: "300px" }}
+                    alt="securityImage"
+                  />
+                </div>
+              </Col>
+            </Row>
           </li>
         </ul>
       </Stack>
