e309636bcd240826f79025ed511226e57729eb86
Acatrinei Ștefan <stefan.aca74@gmail.com>
2024-06-02 23:51:14 +0300

    replaced with new backend project

---
+++ myledger/src/main/java/com/example/myledger/MyledgerApplication.java
@@ -0,0 +1,13 @@
+package com.example.myledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyledgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyledgerApplication.class, args);
+	}
+
+}
